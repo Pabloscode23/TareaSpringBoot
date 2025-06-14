54b5bdb864baa3063d6efd4fa073c30485a8bf2a
Pablo Navarro <pnavarro0923@gmail.com>
2025-06-13 18:51:30 -0600

    feat/implemented rest for category

--- src/main/java/com/project/demo/rest/product/ProductRestController.java
+++ src/main/java/com/project/demo/rest/product/ProductRestController.java
@@ -1,17 +1,24 @@
 package com.project.demo.rest.product;
+import com.project.demo.logic.entity.category.Category;
+import com.project.demo.logic.entity.category.CategoryRepository;
+import com.project.demo.logic.entity.http.GlobalResponseHandler;
 import com.project.demo.logic.entity.product.Product;
 import com.project.demo.logic.entity.product.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
 public class ProductRestController {
     @Autowired
     private ProductRepository productRepository;
+    private CategoryRepository categoryRepository;
 
     @GetMapping
     @PreAuthorize("isAuthenticated()")
@@ -39,6 +46,10 @@ public class ProductRestController {
     @PostMapping
     @PreAuthorize("hasAnyRole('SUPER_ADMIN_ROLE')")
     public Product createProduct(@RequestBody Product product) {
+        Optional<Category> optionalCategory = categoryRepository.findById(product.getCategory().getId());
+     if (optionalCategory.isEmpty()) {
+            throw new RuntimeException("Category not found with id: " + product.getCategory().getId());
+     }
         return productRepository.save(product);
     }
 
